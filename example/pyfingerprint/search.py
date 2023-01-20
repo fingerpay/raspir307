3519a562d77de3935c33603e027ab7dcb04b4684
Colk-tech <master@colk.dev>
2023-01-20 23:47:55 +0900

    [追加] `example/pyfingerprint/search.py` を追加

---
+++ example/pyfingerprint/search.py
@@ -0,0 +1,24 @@
+from typing import Optional, Generator
+
+from fingerdriver.statics.status import GeneratorStatus
+from fingerdriver.models.finger import Finger
+
+from fingerdriver.impls.pyfingerprint.pf import PfDriver
+
+if __name__ == "__main__":
+    driver: PfDriver = PfDriver(port="/dev/serial0")
+
+    driver.ensure_initialized()
+
+    generator: Generator[tuple[GeneratorStatus, None], tuple[GeneratorStatus, Optional[Finger]], None] = \
+        driver.search()
+
+    while True:
+        status: tuple[GeneratorStatus, Optional[Finger]] = next(generator)
+
+        if status[1] is not None:
+            print("Result:\n")
+            print(status[1].template_id, status[1].characteristics_identifier, status[1].confidence)
+
+        if not status[0].should_continue():
+            break
